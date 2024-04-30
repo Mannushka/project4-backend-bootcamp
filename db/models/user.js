e1fdcdb11060287f27e7933ef29d5b7d041ee969
Mannushka <zaimar1910@gmail.com>
2024-05-01 00:01:00 +0800

    create user model and migration

---
+++ db/models/user.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      first_name: DataTypes.STRING,
+      last_name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "user",
+      underscored: true,
+    }
+  );
+  return User;
+};
