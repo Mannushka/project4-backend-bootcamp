5f14ec7f7c314a2df2a0eee0c3daa26727871731
Mannushka <zaimar1910@gmail.com>
2024-05-01 22:56:49 +0800

    define user controller and router

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const app = express();
 require("dotenv").config();
 const port = 3001;
 app.use(cors());
+app.use(express.json());
 //import controllers
-
+const UsersController = require("./controllers/usersController");
 //import routers
+const UsersRouter = require("./routers/usersRouter");
 
 //import DB
-
+const db = require("./db/models/index");
+const { user } = db;
 //initialize controllers
-
+const usersController = new UsersController(user);
 //initialize routers
+const usersRouter = new UsersRouter(usersController).routes();
 
 app.get("/", (req, res) => res.send("Hello world"));
+
+// enable and use router
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
