2b7a5762648d4795f4ca7ebba35b353588a9c45e
Mannushka <zaimar1910@gmail.com>
2024-05-10 22:02:11 +0800

    update location id in restaurants table

---
+++ db/seeders/20240510135705-update-an-choi-restaurant-location.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkUpdate(
+      "restaurants",
+      { location_id: 2 },
+      { id: 3 }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkUpdate(
+      "restaurants",
+      { location_id: 3 },
+      { id: 3 }
+    );
+  },
+};
