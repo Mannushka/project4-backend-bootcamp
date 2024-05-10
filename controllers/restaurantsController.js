2424bc1772b47f2167d6f7d3b2d37bf346bd4745
Mannushka <zaimar1910@gmail.com>
2024-05-10 18:27:07 +0800

    add filters by category, location, price category

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -1,4 +1,5 @@
 const BaseController = require("./baseController");
+const { Op } = require("sequelize");
 
 class RestaurantsController extends BaseController {
   constructor(model, locationModel, food_categoryModel) {
@@ -8,7 +9,7 @@ class RestaurantsController extends BaseController {
   }
   async getAll(req, res) {
     try {
-      const output = await this.model.findAll({
+      let restaurants = await this.model.findAll({
         include: [
           {
             model: this.locationModel,
@@ -20,11 +21,49 @@ class RestaurantsController extends BaseController {
           },
         ],
       });
-      return res.json(output);
+
+      const { location, category, priceCategory } = req.query;
+
+      const filters = {};
+
+      if (location && typeof location === "string") {
+        filters["$location.location_name$"] = {
+          [Op.iLike]: `%${location}%`,
+        };
+      }
+
+      if (category && typeof category === "string") {
+        filters["$food_category.category_name$"] = {
+          [Op.iLike]: `%${category}%`,
+        };
+      }
+
+      if (priceCategory) {
+        filters.price_category = Number(priceCategory);
+      }
+
+      if (Object.keys(filters).length > 0) {
+        restaurants = await this.model.findAll({
+          include: [
+            {
+              model: this.locationModel,
+              attributes: ["location_name"],
+            },
+            {
+              model: this.food_categoryModel,
+              attributes: ["category_name"],
+            },
+          ],
+          where: filters,
+        });
+      }
+
+      return res.json(restaurants);
     } catch (err) {
-      return res.status(400).json({ error: true, msg: err });
+      return res.status(400).json({ error: true, msg: err.message });
     }
   }
+
   async getOne(req, res) {
     const { restaurantId } = req.params;
 
