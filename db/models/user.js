48f231489d9eacf1605a7839323855c39edfa1a2
Mannushka <zaimar1910@gmail.com>
2024-05-13 14:11:52 +0800

    add association with a review model

--- db/models/user.js
+++ db/models/user.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.review, {
+        foreignKey: "user_id",
+      });
     }
   }
   User.init(
