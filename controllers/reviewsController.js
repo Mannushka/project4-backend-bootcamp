5419fc54299fb2c92815c26325e0db68ba96f2c7
Mannushka <zaimar1910@gmail.com>
2024-05-13 14:09:04 +0800

    add validation

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -14,6 +14,11 @@ class ReviewsController extends BaseController {
           .status(400)
           .json({ error: true, msg: "Restaurant ID is required." });
       }
+      if (isNaN(restaurant_id)) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "Restaurant ID must be a number." });
+      }
 
       const reviews = await this.model.findAll({
         where: { restaurant_id: restaurant_id },
@@ -27,21 +32,56 @@ class ReviewsController extends BaseController {
   async postOne(req, res) {
     const { email, restaurant_id, rating_value, text } = req.body;
     try {
-      if (email && restaurant_id && rating_value && text) {
-        const user = await this.userModel.findOrCreate({
-          where: { email: email },
-        });
-        const user_id = user[0].dataValues.id;
-        if (user_id) {
-          const review = await this.model.create({
-            user_id,
-            restaurant_id,
-            rating_value,
-            text,
-          });
-          return res.json(review);
-        }
+      if (!email || !restaurant_id || !rating_value || !text) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "Some values are missing." });
       }
+      const emailRegex = /^\S+@\S+\.\S+$/;
+      if (!emailRegex.test(email)) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "Invalid email format." });
+      }
+      if (isNaN(rating_value)) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "Rating value must be a number." });
+      }
+      if (isNaN(restaurant_id)) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "Restaurant ID must be a number." });
+      }
+      if (text.length < 80) {
+        return res.status(400).json({
+          error: true,
+          msg: "Review must be at least 80 characters long.",
+        });
+      }
+      const user = await this.userModel.findOrCreate({
+        where: { email: email },
+      });
+
+      const user_id = user[0].dataValues.id;
+      if (!user_id) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "User ID is required." });
+      }
+
+      if (isNaN(user_id)) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "User ID must be a number." });
+      }
+      const review = await this.model.create({
+        user_id,
+        restaurant_id,
+        rating_value,
+        text,
+      });
+      return res.json(review);
     } catch (err) {
       console.log(err.message);
       return res.status(400).json({ error: true, msg: err.message });
