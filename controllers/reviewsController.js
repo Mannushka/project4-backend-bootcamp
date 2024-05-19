4f7be8c2d99dc2a0280770c7dd657adae6736d0d
Mannushka <zaimar1910@gmail.com>
2024-05-20 00:44:56 +0800

    edit posting a review function, add deleting a review function

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -14,7 +14,19 @@ class ReviewsController extends BaseController {
       const user_id = user.dataValues.id;
       return user_id;
     } catch (err) {
+      console.log(err.message);
       throw new Error(`Error retrieving user ID: ${err}`);
+    }
+  }
+
+  //does not work
+  async checkIfUserExists(userId) {
+    try {
+      const user = await this.userModel.findByPk(userId);
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      throw new Error("Error checking if user exists: " + error);
     }
   }
 
@@ -28,16 +40,16 @@ class ReviewsController extends BaseController {
   }
 
   async getAllReviewsForRestaurant(req, res) {
-    const { restaurant_id } = req.query;
+    const { restaurantId } = req.query;
     try {
-      if (!restaurant_id || !this.validateNumber(restaurant_id)) {
+      if (!restaurantId || !this.validateNumber(restaurantId)) {
         return res
           .status(400)
           .json({ error: true, msg: "Invalid or missing restaurant ID." });
       }
 
       const reviews = await this.model.findAll({
-        where: { restaurant_id: restaurant_id },
+        where: { restaurant_id: restaurantId },
         include: [
           {
             model: this.userModel,
@@ -57,29 +69,43 @@ class ReviewsController extends BaseController {
   }
 
   async getAllReviewsByUser(req, res) {
-    const { email } = req.query;
+    // const { email } = req.query;
+    const { userId } = req.query;
     try {
-      if (!email || !this.validateEmail(email)) {
-        return res
-          .status(400)
-          .json({ error: true, msg: "Invalid or missing user email." });
-      }
+      // if (!email || !this.validateEmail(email)) {
+      //   return res
+      //     .status(400)
+      //     .json({ error: true, msg: "Invalid or missing user email." });
+      // }
 
-      const user = await this.userModel.findOrCreate({
-        where: { email: email },
-      });
+      // const user = await this.userModel.findOrCreate({
+      //   where: { email: email },
+      // });
 
-      const user_id = await this.getUserIdByEmail(this.userModel, email);
+      // const userId = await this.getUserIdByEmail(this.userModel, email);
 
-      if (!user_id || !this.validateNumber(user_id)) {
+      if (!userId || !this.validateNumber(userId)) {
         return res
           .status(400)
           .json({ error: true, msg: "Invalid or missing user ID." });
       }
 
+      // const isUserInDb = await checkIfUserExists(this.userModel, userId);
+      // if (!isUserInDb) {
+      //   return res.status(404).json({ error: true, msg: "User not found." });
+      // }
+      // console.log(await checkIfUserExists(userId));
+      const user = await this.userModel.findByPk(userId);
+      if (!user) {
+        throw new Error("User does not exist");
+      }
       const reviews = await this.model.findAll({
-        where: { user_id: user_id },
+        where: { user_id: userId },
         include: [
+          {
+            model: this.userModel,
+            attributes: ["first_name"],
+          },
           {
             model: this.review_photoModel,
             attributes: ["id", "photo"],
@@ -94,31 +120,44 @@ class ReviewsController extends BaseController {
   }
 
   async postOne(req, res) {
-    const { email, restaurant_id, rating_value, text, photoURLs } = req.body;
+    const { userId, restaurantId, rating_value, text, photoURLs } = req.body;
     try {
       const result = await sequelize.transaction(async (t) => {
-        if (!email || !restaurant_id || !rating_value || !text) {
+        if (!userId || !restaurantId || !rating_value || !text) {
           return res
             .status(400)
             .json({ error: true, msg: "Some values are missing." });
         }
 
-        if (!this.validateEmail(email)) {
+        // if (!this.validateEmail(email)) {
+        //   return res
+        //     .status(400)
+        //     .json({ error: true, msg: "Invalid email format." });
+        // }
+
+        if (!this.validateNumber(userId)) {
           return res
             .status(400)
-            .json({ error: true, msg: "Invalid email format." });
+            .json({ error: true, msg: "User ID must be a number." });
         }
-
+        const user = await this.userModel.findByPk(userId);
+        if (!user) {
+          throw new Error("User does not exist");
+        }
         if (!this.validateNumber(rating_value)) {
           return res
             .status(400)
             .json({ error: true, msg: "Rating value must be a number." });
         }
 
-        if (!this.validateNumber(restaurant_id)) {
+        if (!this.validateNumber(restaurantId)) {
           return res
             .status(400)
             .json({ error: true, msg: "Restaurant ID must be a number." });
+        }
+        const restaurant = await this.restaurantModel.findByPk(userId);
+        if (!restaurant) {
+          throw new Error("Restaurant does not exist");
         }
 
         if (text.length < 80) {
@@ -128,20 +167,12 @@ class ReviewsController extends BaseController {
           });
         }
 
-        const user_id = await this.getUserIdByEmail(this.userModel, email);
-
-        if (!user_id || !this.validateNumber(user_id)) {
-          return res
-            .status(400)
-            .json({ error: true, msg: "Invalid or missing user ID." });
-        }
-
         const review = await this.model.create(
           {
-            user_id,
-            restaurant_id,
-            rating_value,
-            text,
+            user_id: userId,
+            restaurant_id: restaurantId,
+            rating_value: rating_value,
+            text: text,
           },
           { transaction: t }
         );
@@ -163,6 +194,47 @@ class ReviewsController extends BaseController {
       return res.json(result);
     } catch (err) {
       console.log(err.message);
+      return res.status(400).json({ error: true, msg: err.message });
+    }
+  }
+
+  async deleteReview(req, res) {
+    const { userId } = req.query;
+    const { reviewId } = req.params;
+
+    try {
+      if (!userId || !this.validateNumber(userId)) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "Invalid or missing user ID." });
+      }
+
+      if (!reviewId || !this.validateNumber(reviewId)) {
+        return res
+          .status(400)
+          .json({ error: true, msg: "Invalid or missing review ID." });
+      }
+
+      const review = await this.model.findOne({
+        where: { id: reviewId, user_id: userId },
+      });
+
+      if (!review) {
+        return res
+          .status(404)
+          .json({ error: true, msg: "Review not found for the user." });
+      }
+
+      await review.destroy();
+      const isReviewDeleted = await this.model.findOne({
+        where: { id: reviewId, user_id: userId },
+      });
+      console.log(isReviewDeleted);
+      if (!isReviewDeleted) {
+        return res.json({ success: true, msg: "Review deleted successfully." });
+      }
+    } catch (err) {
+      console.error(err);
       return res.status(400).json({ error: true, msg: err.message });
     }
   }
