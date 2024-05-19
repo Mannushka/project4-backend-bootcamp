2963dce03323bb442957122421fbc3e68c80c794
Mannushka <zaimar1910@gmail.com>
2024-05-19 16:03:24 +0800

    define routes for requests to create/delete/get saved_restaurants

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -8,12 +8,23 @@ class UsersRouter {
   routes() {
     router.get("/", this.controller.getAll.bind(this.controller));
     router.get(
-      "/finduser",
+      "/find-user",
       this.controller.getUserByEmail.bind(this.controller)
     );
     router.post("/", this.controller.postUser.bind(this.controller));
     router.delete("/:userId", this.controller.deleteUser.bind(this.controller));
-
+    router.post(
+      "/:userId/my-restaurants",
+      this.controller.createSavedRestaurant.bind(this.controller)
+    );
+    router.get(
+      "/:userId/my-restaurants",
+      this.controller.getSavedRestaurants.bind(this.controller)
+    );
+    router.delete(
+      "/:userId/my-restaurants",
+      this.controller.deleteSavedRestaurant.bind(this.controller)
+    );
     return router;
   }
 }
