df8468bf11c75005f33067f6623a60bf34a38e3d
Mannushka <zaimar1910@gmail.com>
2024-05-20 22:08:38 +0800

    add pa gination when getting all restaurants

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -8,7 +8,100 @@ class RestaurantsController extends BaseController {
     this.food_categoryModel = food_categoryModel;
     this.reviewModel = reviewModel;
   }
+  // async getAll(req, res) {
+  //   try {
+  //     let restaurants = await this.model.findAll({
+  //       include: [
+  //         {
+  //           model: this.locationModel,
+  //           attributes: ["location_name"],
+  //         },
+  //         {
+  //           model: this.food_categoryModel,
+  //           attributes: ["category_name"],
+  //         },
+  //         {
+  //           model: this.reviewModel,
+  //           attributes: ["rating_value"],
+  //         },
+  //       ],
+  //     });
+
+  //     const { location, category, priceCategory } = req.query;
+
+  //     const filters = {};
+  //     if (location && Array.isArray(location)) {
+  //       console.log(location);
+  //       filters["$location.location_name$"] = {
+  //         [Op.or]: location.map((location) => ({
+  //           [Op.iLike]: `%${location}%`,
+  //         })),
+  //       };
+  //     } else if (location && typeof location === "string") {
+  //       filters["$location.location_name$"] = {
+  //         [Op.iLike]: `%${location}%`,
+  //       };
+  //     }
+
+  //     if (category && Array.isArray(category)) {
+  //       console.log(category);
+  //       filters["$food_category.category_name$"] = {
+  //         [Op.or]: category.map((category) => ({
+  //           [Op.iLike]: `%${category}%`,
+  //         })),
+  //       };
+  //     } else if (category && typeof category === "string") {
+  //       filters["$food_category.category_name$"] = {
+  //         [Op.iLike]: `%${category}%`,
+  //       };
+  //     }
+
+  //     if (priceCategory) {
+  //       if (Array.isArray(priceCategory)) {
+  //         const isPriceCategoryValid = priceCategory.every((category) =>
+  //           [1, 2, 3].includes(Number(category))
+  //         );
+  //         if (isPriceCategoryValid) {
+  //           filters.price_category = {
+  //             [Op.in]: priceCategory.map(Number),
+  //           };
+  //         }
+  //       } else if (
+  //         !isNaN(priceCategory) &&
+  //         [1, 2, 3].includes(Number(priceCategory))
+  //       ) {
+  //         filters.price_category = Number(priceCategory);
+  //       }
+  //     }
+
+  //     if (Object.keys(filters).length > 0) {
+  //       restaurants = await this.model.findAll({
+  //         include: [
+  //           {
+  //             model: this.locationModel,
+  //             attributes: ["location_name"],
+  //           },
+  //           {
+  //             model: this.food_categoryModel,
+  //             attributes: ["category_name"],
+  //           },
+  //           {
+  //             model: this.reviewModel,
+  //             attributes: ["rating_value"],
+  //           },
+  //         ],
+  //         where: filters,
+  //       });
+  //     }
+
+  //     return res.json(restaurants);
+  //   } catch (err) {
+  //     return res.status(400).json({ error: true, msg: err.message });
+  //   }
+  // }
+
   async getAll(req, res) {
+    const { page = 1, pageSize = 5 } = req.query;
     try {
       let restaurants = await this.model.findAll({
         include: [
@@ -25,6 +118,8 @@ class RestaurantsController extends BaseController {
             attributes: ["rating_value"],
           },
         ],
+        limit: pageSize,
+        offset: (page - 1) * pageSize,
       });
 
       const { location, category, priceCategory } = req.query;
@@ -94,7 +189,31 @@ class RestaurantsController extends BaseController {
         });
       }
 
-      return res.json(restaurants);
+      const totalCount = await this.model.count({
+        // include: [
+        //   {
+        //     model: this.locationModel,
+        //     attributes: ["location_name"],
+        //   },
+        //   {
+        //     model: this.food_categoryModel,
+        //     attributes: ["category_name"],
+        //   },
+        //   {
+        //     model: this.reviewModel,
+        //     attributes: ["rating_value"],
+        //   },
+        // ],
+        where: filters,
+      });
+
+      // return res.json(restaurants);
+      return res.json({
+        restaurants,
+        totalCount,
+        totalPages: Math.ceil(totalCount / pageSize),
+        currentPage: Number(page),
+      });
     } catch (err) {
       return res.status(400).json({ error: true, msg: err.message });
     }
