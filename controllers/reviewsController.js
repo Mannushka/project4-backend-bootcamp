c6e0cd4ccb3e28205d352c418752b5097afac4ac
Mannushka <zaimar1910@gmail.com>
2024-06-17 14:00:35 +0800

    delete some unused code

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -110,32 +110,14 @@ class ReviewsController extends BaseController {
   }
 
   async getAllReviewsByUser(req, res) {
-    // const { email } = req.query;
     const { userId } = req.query;
     try {
-      // if (!email || !this.validateEmail(email)) {
-      //   return res
-      //     .status(400)
-      //     .json({ error: true, msg: "Invalid or missing user email." });
-      // }
-
-      // const user = await this.userModel.findOrCreate({
-      //   where: { email: email },
-      // });
-
-      // const userId = await this.getUserIdByEmail(this.userModel, email);
-
       if (!userId || !this.validateNumber(userId)) {
         return res
           .status(400)
           .json({ error: true, msg: "Invalid or missing user ID." });
       }
 
-      // const isUserInDb = await checkIfUserExists(this.userModel, userId);
-      // if (!isUserInDb) {
-      //   return res.status(404).json({ error: true, msg: "User not found." });
-      // }
-      // console.log(await checkIfUserExists(userId));
       const user = await this.userModel.findByPk(userId);
       if (!user) {
         throw new Error("User does not exist");
@@ -174,12 +156,6 @@ class ReviewsController extends BaseController {
             .status(400)
             .json({ error: true, msg: "Some values are missing." });
         }
-
-        // if (!this.validateEmail(email)) {
-        //   return res
-        //     .status(400)
-        //     .json({ error: true, msg: "Invalid email format." });
-        // }
 
         if (!this.validateNumber(userId)) {
           return res
@@ -313,104 +289,6 @@ class ReviewsController extends BaseController {
       res.status(400).json({ error: true, msg: err.message });
     }
   }
-  // async getAllReviewsByUser(req, res) {
-  //   const { email } = req.query;
-  //   try {
-  //     if (!email) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "User email is required." });
-  //     }
-  //     const emailRegex = /^\S+@\S+\.\S+$/;
-  //     if (!emailRegex.test(email)) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "Invalid email format." });
-  //     }
-  //     const user = await this.userModel.findOrCreate({
-  //       where: { email: email },
-  //     });
-
-  //     const user_id = user[0].dataValues.id;
-  //     if (!user_id) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "User ID is required." });
-  //     }
-
-  //     if (isNaN(user_id)) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "User ID must be a number." });
-  //     }
-
-  //     const reviews = await this.model.findAll({
-  //       where: { user_id: user_id },
-  //     });
-  //     return res.json(reviews);
-  //   } catch (err) {
-  //     return res.status(400).json({ error: true, msg: err });
-  //   }
-  // }
-
-  // async postOne(req, res) {
-  //   const { email, restaurant_id, rating_value, text } = req.body;
-  //   try {
-  //     if (!email || !restaurant_id || !rating_value || !text) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "Some values are missing." });
-  //     }
-  //     const emailRegex = /^\S+@\S+\.\S+$/;
-  //     if (!emailRegex.test(email)) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "Invalid email format." });
-  //     }
-  //     if (isNaN(rating_value)) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "Rating value must be a number." });
-  //     }
-  //     if (isNaN(restaurant_id)) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "Restaurant ID must be a number." });
-  //     }
-  //     if (text.length < 80) {
-  //       return res.status(400).json({
-  //         error: true,
-  //         msg: "Review must be at least 80 characters long.",
-  //       });
-  //     }
-  //     const user = await this.userModel.findOrCreate({
-  //       where: { email: email },
-  //     });
-
-  //     const user_id = user[0].dataValues.id;
-  //     if (!user_id) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "User ID is required." });
-  //     }
-
-  //     if (isNaN(user_id)) {
-  //       return res
-  //         .status(400)
-  //         .json({ error: true, msg: "User ID must be a number." });
-  //     }
-  //     const review = await this.model.create({
-  //       user_id,
-  //       restaurant_id,
-  //       rating_value,
-  //       text,
-  //     });
-  //     return res.json(review);
-  //   } catch (err) {
-  //     console.log(err.message);
-  //     return res.status(400).json({ error: true, msg: err.message });
-  //   }
-  // }
 }
 
 module.exports = ReviewsController;
